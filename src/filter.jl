499ae1bb4c92883ac487301faff9a8faeb458c55
Frank Schaefer <frank.schaefer@unibas.ch>
2021-02-22 21:03:58 +0100

    number conflicts with diagonal

--- src/filter.jl
+++ src/filter.jl
@@ -5,7 +5,7 @@ mypack(a::Number...) = [a...]
 mypack(a::SArray,b::SArray,c::Number) = ArrayPartition(a,b,@SVector[c])
 mypack(a::SArray,b::SArray,c::SArray) = ArrayPartition(a,b,c)
 
-compute_dP(B,P,σtil::Number) = B*P + P*B' .- outer_(σtil)
+compute_dP(B,P,σtil::Number) = B*P + P*B' .- σtil*σtil'
 compute_dP(B,P,σtil) = B*P + P*B' - outer_(σtil)
 compute_dν(B,ν,β::Number) = B*ν .+ β
 compute_dν(B,ν,β) = B*ν + β
