29d3ebfc792316f522018d5bcf5ca8474e846949
mschauer <moritzschauer@web.de>
2021-03-31 20:13:38 +0200

    Fixes?

--- example/phylo/phylo2.jl
+++ example/phylo/phylo2.jl
@@ -11,6 +11,7 @@ using Makie
 using NewickTree
 using OrdinaryDiffEq
 using DiffEqNoiseProcess
+using Test
 
 const d = 2
 const 𝕏 = SVector{d,Float64}
@@ -19,15 +20,15 @@ const MSDE = MitosisStochasticDiffEq
 
 
 ## define model (two traits, as in presentation)
-θ = ([3.0, 1.2], [0.1, 0.1])
-const M = -0.05I + [-1.0 1.0; 1.0 -1.0]
+θtrue = ([0.5, 1.2], [0.1, 0.1])
+const M = -0.001I + [-1.0 1.0; 1.0 -1.0]
 
 B(θ) = Diagonal(θ[1]) * M
 Σ(θ) = Diagonal(θ[2])
 
 f(u,θ,t) = tanh.(Diagonal(θ[1]) * M * u)
 g(u,θ,t) = θ[2]
-
+θ = θtrue
 θlin = (B(θ), zeros(d), Σ(θ))
 
 ## tree
@@ -65,7 +66,6 @@ function Tree(S::String)
         T[node.id] = T[p] + node.data.distance
     end
     @assert all(Par .< 1:n)
-
     lastone = zeros(Bool,n)
     parentdone = zeros(Bool,n)
     for i ∈ n:-1:2
@@ -86,7 +86,8 @@ S = "(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F;"
 S20 = "(((t20:0.1377639426,t8:0.3216105914):0.461882598,(((t14:0.09650261351,t12:0.5344695284):0.1058950284,(t1:0.7008816295,t13:0.8587384557):0.7271672823):0.8026451592,(t6:0.5240857233,(t16:0.451590274,t10:0.4654036982):0.8569358122):0.140687139):0.7207935331):0.06727131596,((((t17:0.03115223325,t18:0.8930883685):0.6862687438,t9:0.4896655723):0.3056753092,t3:0.2549535078):0.07862622454,(((t4:0.03973490326,t5:0.4096179632):0.3020580804,t15:0.06550096255):0.2660011558,(t7:0.3895205685,((t19:0.165174433,t2:0.6469203965):0.2730371242,t11:0.2700119715):0.8393620371):0.02117527928):0.206376995):0.6012785432);"
 S50 = "((((((t23:0.5766009684,t33:0.9917489749):0.3572315329,(t40:0.4535627225,t25:0.1296473157):0.9540394924):0.4740875189,(t6:0.946340692,t47:0.386682119):0.1535291784):0.07034701738,((((t49:0.6612032412,t27:0.1606025242):0.1527153626,t15:0.1854858669):0.5782065149,t50:0.3087045723):0.0759275395,((t17:0.258283512,t35:0.7651973176):0.2003453802,t2:0.4033703087):0.2418845498):0.6834833182):0.4268306631,(((t26:0.1744227773,(t16:0.6275201498,t10:0.09013780276):0.1857208994):0.8804157388,(t28:0.626011228,t46:0.8026940082):0.7066663445):0.6334459039,(t45:0.3075582262,t42:0.2759609232):0.05279792193):0.4003514573):0.5333229457,((((t30:0.05955170072,t41:0.2500256761):0.04172409023,((t19:0.3332289669,t31:0.6650944015):0.2450174955,t9:0.3372890623):0.2041792406):0.3337947708,t1:0.9396123393):0.0896910606,(((((t12:0.1379979164,t43:0.9007850592):0.703310129,(t11:0.3347885907,(t22:0.7190679635,t14:0.8988074451):0.1574828231):0.9644375315):0.137505424,((t37:0.7556577872,t8:0.3481238757):0.7712922962,(t7:0.7946650374,t48:0.8638419164):0.338934626):0.7507952554):0.5193078597,(t4:0.2408366969,(((t29:0.7862726359,t38:0.4319011718):0.8655222375,((t20:0.09156441619,t3:0.5975058537):0.3240908289,(t34:0.7324223334,t32:0.1348467385):0.4636785307):0.7708284601):0.7991992962,t39:0.2996040138):0.4087550903):0.1688272723):0.7225795928,(((t44:0.02794037666,t5:0.09033886855):0.5778779227,(t24:0.8744501146,((t21:0.2940180032,t18:0.03399693617):0.8231791619,t13:0.5192477896):0.5669933448):0.9547001948):0.8561708115,t36:0.9225172002):0.02867545444):0.1595393196):0.1254606678);"
 
-tree = Tree(S20)
+tree = Tree(S50)
+@test sort(setdiff(1:length(tree.Par), tree.Par)) == sort(tree.lids)
 print_tree(tree.newick)
 # fieldnames(typeof(nwtree))
 
@@ -116,7 +117,7 @@ Xd = [u0]    # save endpoints
 segs = Vector{Any}(undef, tree.n) # save all guided segments
 
 for i in eachindex(tree.T)
-    local κ, x, xT, dt
+    local κ, x, xT, dt, u
     i == 1 && continue  # skip root-node  (has no parent)
 
     # define timegrid on segment
@@ -124,7 +125,7 @@ for i in eachindex(tree.T)
     t = tree.T[i′]
     m = round(Int, (tree.T[i]-t)/dt0)
     dt = (tree.T[i] - t)/m
-
+    
     u = Xd[i′]   # starting value
     κ = MSDE.SDEKernel(f, g, t:dt:tree.T[i], θ)
     x, xT = MSDE.sample(κ, u; save_noise=true)
@@ -173,23 +174,55 @@ function bwfiltertree!(Q, tree::Tree, θlin, dt0)
 end
 
 
+"""
+    pCN(noise::AbstractNoiseProcess, ρ; reset=true,reverse=false,indx=nothing)
+Create a new, but correlated noise process from `noise` and additional entropy with correlation ρ.
+"""
+function DiffEqNoiseProcess.pCN(source::DiffEqNoiseProcess.AbstractNoiseProcess{T,N,Vector{T2},inplace}, ρ;
+              reset=true,reverse=false,indx=nothing) where {T,N,T2,inplace}
+
+  source′ = deepcopy(source)
+  rng = Random.GLOBAL_RNG
+  # generate new Wiener process similar to the one in source
+  t = source′.t
+  if typeof(source′.W[1]) <: Number
+    Wnew = cumsum([zero(source′.W[1]);[sqrt.(t[i+1]-ti).*DiffEqNoiseProcess.wiener_randn(rng,typeof(source′.W[i]))
+              for (i,ti) in enumerate(t[1:end-1])]])
+  else
+    Wnew = cumsum([[zero(source′.W[1])];[sqrt.(t[i+1]-ti).*DiffEqNoiseProcess.wiener_randn(rng,source′.W[i])
+              for (i,ti) in enumerate(t[1:end-1])]])
+  end
+
+  source′.W = ρ * source′.W + sqrt(one(ρ)-ρ^2) * Wnew
+  source′.u = ρ * source′.u + sqrt(one(ρ)-ρ^2) * Wnew
+  #NoiseGrid(t, Vector.(W))
+  NoiseWrapper(source′,reset=reset,reverse=reverse,indx=indx)
+end
 
 ## simulate conditioned process using messages
 
-function fwguidtree!(messages, X, guidedsegs, ll, tree::Tree, f, g, θ, rho)
+function fwguidtree!(messages, X, guidedsegs, noisesegs, ll, tree::Tree, f, g, θ, rho)
     T = tree.T
+    ll .= -1
+    ll[1] = 0.0
+    if isnothing(noisesegs)
+        noisesegs = guidedsegs
+    end
     for i in eachindex(T)
         i == 1 && continue  # skip root-node (has no parent)
         i′ = tree.Par[i]
+        @assert i′ < i
+    #    @show i′, i
+        @assert ll[i′] != -1
         llu = ll[i'] * tree.lastone[i]
         κ = MSDE.SDEKernel(f, g, messages[i].ts, θ)
-        if rho == 0 || !isassigned(guidedsegs, i)
+        if rho == 0 || !isassigned(noisesegs, i)
             Z = nothing
         else
-            if guidedsegs[i].W isa DiffEqNoiseProcess.NoiseProcess
-                Z = pCN(guidedsegs[i].W, rho) # think about modifying?
+            if noisesegs[i].W isa DiffEqNoiseProcess.NoiseProcess || noisesegs[i].W isa NoiseGrid
+                Z = pCN(noisesegs[2].W, rho)
             else
-                Z = pCN(guidedsegs[i].W.source, rho)
+                Z = pCN(noisesegs[2].W.source, rho)
             end
         end
         solfw, llnew = MSDE.forwardguiding(κ, messages[i], (X[i′], llu), Z; inplace=false, save_noise=true)
@@ -197,6 +230,7 @@ function fwguidtree!(messages, X, guidedsegs, ll, tree::Tree, f, g, θ, rho)
         X[i] = 𝕏(solfw[end][1:end-1])
         guidedsegs[i] = solfw
     end
+    @assert !any(isnan.(ll))
     ll_leaves = sum(ll[tree.lids])
     X, guidedsegs, ll, ll_leaves
 end
@@ -217,35 +251,34 @@ Q, messages = bwfiltertree!(Q, tree, θlin, dt0)
 
 # init, set types
 guidedsegs = Vector{Any}(undef, tree.n) # save all guided segments
-X, guidedsegs, ll, ll_leaves = fwguidtree!(messages, X, guidedsegs, zeros(tree.n), tree, f, g, θ, rho)
+X, guidedsegs, ll, ll_leaves = fwguidtree!(messages, X, guidedsegs, nothing, zeros(tree.n), tree, f, g, θ, rho)
 G = typeof(guidedsegs[2])
 
 # small test if pCN works
 ll2 = zeros(tree.n)
-X, guidedsegs2, ll2, ll_leaves2 = fwguidtree!(messages, X, copy(guidedsegs), ll2, tree, f, g, θ, 1.0)
+X, guidedsegs2, ll2, ll_leaves2 = fwguidtree!(messages, X, copy(guidedsegs), nothing, ll2, tree, f, g, θ, 1.0)
 
 @show ll-ll2 # expected: all zeros
 # @show hcat(guidedsegs[3].u- guidedsegs2[3].u) # expected: all zeros
 
-guidedsegs[2].W.W == pCN(guidedsegs[2].W, 1.0).source.W
+#@test guidedsegs[2].W.W == pCN(guidedsegs[2].W, 1.0).source.W
 
+W = guidedsegs[2].W
+u = copy(W.u)
+W2 = pCN(W, .0)
+#@test_broken W.u != W2.source.u
 
 ## plotting
-
-cols = repeat([:blue, :red, :magenta, :orange],2)
-pl = scatter(tree.T, getindex.(X,1),color=:red)
-scatter!(tree.T, getindex.(X,2),color=:black)
-for j ∈ 1:d
-    #pl = scatter(T, getindex.(X,j),color=:black)
+function plot(tree, X, guidedsegs, j=1)
+    cols = repeat([:blue, :red, :magenta, :orange],2)
+    pl = scatter(tree.T, getindex.(X,j),color=:red)
     for i in 2:tree.n
         gg = guidedsegs[i]
-        #gg = segs[i]
-        #lines!(gg.t, getindex.(gg.u,j),color=cols[i])
-        lines!(gg.t, getindex.(gg.u,j),color=sample(cols))
+        lines!(gg.t, getindex.(gg.u,j), color=sample(cols))
     end
-    save("treecomp$j.png", pl)
+    pl
 end
-display(pl)
+
 
 
 ## regression of drift parameters θ[1]
@@ -280,45 +313,61 @@ p̂ = mean(G)
 se = sqrt.(diag(cov(G)))
 display(map((p̂, se, p) -> "$(round(p̂, digits=3)) ± $(round(se, digits=3)) (true: $p)", p̂, se, θ[1]))
 
+r(x) = round(x, digits=3)
 
-function mcmc(θ, prior, iters, X, tree; dt0 = 0.001, rho=0.9)
+function mcmc(θ, prior, iters, Xd, tree; dt0 = 0.001, σθ=0.1, rho=0.8)
+    X = zeros(𝕏, tree.n) 
+    Xprop = zeros(𝕏, tree.n)  # values at nodes
+    for id in tree.lids
+        X[id] = Xd[id]
+        Xprop[id] = Xd[id]
+    end
+
+
     θs = [θ]
     n = tree.n
-    Q = [i in tree.lids ? WGaussian{(:μ,:Σ,:c)}(Vector(X[i]), 0.01Matrix(I(d)), 0.0) : missing for i in eachindex(X)]
+    Q = [i in tree.lids ? WGaussian{(:μ,:Σ,:c)}(Vector(Xd[i]), 0.002Matrix(I(d)), 0.0) : missing for i in eachindex(Xd)]
 
+    global guidedsegs, X, ll, lls
     guidedsegs = Vector{Any}(undef, n) # save all guided segments
-    ll = 0.0
-    llprop = 0.0
+    guidedsegsprop = Vector{Any}(undef, n) # save all guided segments
+    lls, ll = zeros(n), 0.0
+    llprops, llprob = zeros(n), 0.0
 
     θlin = (B(θ), zeros(d), Σ(θ))
     Q, messages = bwfiltertree!(Q, tree, θlin, dt0)
 
-    X, guidedsegs, _, ll = fwguidtree!(messages, X, guidedsegs, ll, tree, f, g, θ, 0.0)
+    X, guidedsegs, lls, ll = fwguidtree!(messages, X, guidedsegs, nothing, lls, tree, f, g, θ, 0.0)
+    Xprop, guidedsegsprop, lls, ll = fwguidtree!(messages, Xprop, guidedsegs, nothing, lls, tree, f, g, θ, 0.0)
+    
     accepted = 0
-
     for iter in 1:iters
-        mod(iter, 10) == 0 && println(iter)
-
-        guidedsegs2 = copy(guidedsegs)
+        #Q, messages = bwfiltertree!(Q, tree, θlin, dt0)
+      
         #
         # G = driftparamstree(prior, guidedsegs, tree, f, g, θ, paramjac, messages)
         # θ = (rand(convert(Gaussian{(:μ, :Σ)}, G)), θ[2])
-        θprop = (θ[1] + 0.01*randn(size(θ[1])...), θ[2])
-        Xprop, guidedsegsprop, _, llprop = fwguidtree!(messages, X, guidedsegs2, llprop, tree, f, g, θprop, rho)
-        push!(θs, θ)
-        if log(rand()) < llprop - ll
+       # llprops .= 0
+        θprop = (θ[1] + σθ*randn(size(θ[1])...), θ[2])
+        Xprop, guidedsegsprop, llprops, llprop = fwguidtree!(messages, Xprop, guidedsegsprop, guidedsegs, llprops, tree, f, g, θprop, rho)
+        if mod(iter, 10) == 0 
+            println(iter, " acc ", r.(accepted/iter), " θ ", r.(θ[1]), " ll ", r(ll), " llᵒ ", r(llprop))
+        end
+        if  rand() <= exp(llprop - ll)
             ll = llprop
             θ = θprop
-            guidedsegs .= guidedsegsprop
-            X .= Xprop
+            guidedsegs, guidedsegsprop = guidedsegsprop, guidedsegs
+            X, Xprop = Xprop, X
             accepted += 1
         end
+        push!(θs, θ)
+     
     end
     θs, accepted/iters
 end
 iters = 5000
-θinit = ([3.3, 1.5], [0.1, 0.1])
-θs, acc = mcmc(θinit, prior, iters, X, tree;dt0=0.01)
+θinit = (0.9*θtrue[1], θtrue[2])
+θs, acc = mcmc(θinit, prior, iters, Xd, tree; σθ=0.01, rho=0.995, dt0=0.001)
 
 PLOT = true
 if PLOT
